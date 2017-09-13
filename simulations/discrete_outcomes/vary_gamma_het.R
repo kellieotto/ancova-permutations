c584ba9ee7fed5b560ecd6bec6186ecab5e9b442
Kellie Ottoboni <kellieotto@berkeley.edu>
2017-09-13 11:51:29 -0700

    Add discrete outcome simulations and results

---
+++ simulations/discrete_outcomes/vary_gamma_het.R
@@ -0,0 +1,27 @@
+source("simulation_utils.R")
+library(doParallel)
+
+registerDoParallel(cores=30)
+
+gamma_vec <- seq(0, 0.2, by=0.05)
+gamma_res <- foreach(i = seq_along(gamma_vec)) %dopar% {
+  set.seed(760682460) # Generated from random.org Timestamp: 2016-11-14 10:21:12 UTC
+  tmp <- generate_simulated_data(gamma = gamma_vec[i], effect = "heterogeneous", errors = "normal")
+  pvalues <- replicate(10000, {
+    tmp$Z <- permute_within_groups(tmp$Z, tmp$stratumID)
+    tmp$Y1 <-gen_y(gamma = gamma_vec[i], v=tmp$v, error=tmp$delta, tmp$Z)
+    generate_simulated_pvalues(tmp, reps=1e3)
+  })
+  return(pvalues)
+}
+
+gamma_res_het <- lapply(gamma_res, t)
+
+tr_effect_het <- rep(0, 5)
+for( i in seq_along(gamma_vec)){
+  set.seed(760682460) # Generated from random.org Timestamp: 2016-11-14 10:21:12 UTC
+  tmp <- generate_simulated_data(gamma = gamma_vec[i], effect = "same effect", errors = "normal")
+  tr_effect_het[i] <- mean(gamma_vec[i] * exp(tmp$v))
+}
+
+save(tr_effect_het, gamma_res_het, file="vary_gamma_het_results.Rda")
